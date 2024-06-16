7f9bd3f69af1ea190f6dc329289b6ab1ec77b4fa
Herman <arzergerman@gmail.com>
2024-06-16 12:10:39 +0300

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+num1 = int(input("Please, enter the number: "))
+num2 = int(input("Please, enter the number again : "))
+
+print("Result:", num1 + num2)
+print("Result:", num1 - num2)
+print("Result:", num1 * num2)
+print("Result:", num1 / num2)
+print("Result:", num1 ** num2)
+print("Result:", num1 // num2)
